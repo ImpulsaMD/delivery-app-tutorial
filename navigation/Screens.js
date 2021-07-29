7887a499e23b3204dd1e419697ec44ee6c7125a3
Kenny Juca <jucakenny@gmail.com>
2021-07-29 18:10:11 -0500

    tutorial 10: iconos en el tab bar

--- navigation/Screens.js
+++ navigation/Screens.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { View } from "react-native";
-import { Button, Text } from "native-base";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons, FontAwesome } from '@expo/vector-icons'
+
 //screens
 import Home from "../views/Home/Home";
 import AccountScreen from "./AccountScreen";
@@ -15,24 +15,40 @@ const Screens = createBottomTabNavigator(
       screen: Home,
       navigationOptions: {
         title: "Inicio",
+        tabBarIcon: ({ focused, tintColor }) => {
+          const iconName = `restaurant${focused ? '' : '-outline'}`;
+          return <Ionicons name={iconName} size={20} color={tintColor} />
+        }
       },
     },
     RestaurantScreen: {
       screen: RestaurantScreen,
       navigationOptions: {
         title: "Restaurantes",
+        tabBarIcon: ({ focused, tintColor }) => {
+          const iconName = `home${focused ? '-sharp' : '-outline'}`;
+          return <Ionicons name={iconName} size={20} color={tintColor} />
+        }
       },
     },
     CartScreen: {
       screen: CartScreen,
       navigationOptions: {
         title: "Compras",
+        tabBarIcon: ({ focused, tintColor }) => {
+          //const iconName = `home${focused ? '-sharp' : '-outline'}`;
+          return <FontAwesome name='shopping-basket' size={20} color={tintColor} />
+        }
       },
     },
     AccountScreen: {
       screen: AccountScreen,
       navigationOptions: {
         title: "Cuenta",
+        tabBarIcon: ({ focused, tintColor }) => {
+          const iconName = `user${focused ? '' : '-o'}`;
+          return <FontAwesome name={iconName} size={20} color={tintColor} />
+        }
       },
     },
   },
